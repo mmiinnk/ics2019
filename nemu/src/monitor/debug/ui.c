966e3472e41e9ada9d9ed637855d7ae4f2e0f655
tracer-ics2019 <tracer@njuics.org>
2019-09-11 13:45:00 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  13:45:00 up 48 min,  5 users,  load average: 0.62, 0.48, 0.36 267fb90c8e26923f53bd3234fc10692710138532

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,11 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_single_step(char *args){
+	cpu_exec(1);
+	return 0;
+}
+
 static struct {
   char *name;
   char *description;
@@ -46,6 +51,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Single Step", cmd_single_step},
 
   /* TODO: Add more commands */
 
