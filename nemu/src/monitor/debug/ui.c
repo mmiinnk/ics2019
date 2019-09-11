836997478bcc0229ede0911a6a4aca11e167430c
tracer-ics2019 <tracer@njuics.org>
2019-09-11 20:56:42 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:56:42 up  1:50,  5 users,  load average: 0.00, 0.00, 0.00 24249ac815b902463c64772981c2fbe158010d3

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -82,7 +82,7 @@ unsigned int htd(char *str){
 }
 
 static int cmd_scan_mem(char *args){
-	//int n;
+	int n = 0;
 	unsigned int address;
 	char *n_char = strtok(NULL, " ");
 	if (n_char == NULL)
@@ -92,12 +92,30 @@ static int cmd_scan_mem(char *args){
 		if (*(n_char) == '0' && *(n_char+1) == 'x' && strlen(n_char) == 8)
 		{
 			address = htd(n_char);
-			printf("%x\n",address);
-			printf("%s: 0x%x\n",n_char, pmem[address]);
+			//printf("%x\n",address);
+			printf("%s: 0x",n_char);
+			for (int i=0;i<4;i++)
+				printf("%x",pmem[address+i]);
+			printf("\n");
 		}
 		else
 		{
-
+			char *address_char = strtok(NULL, " ");
+			address = htd(address_char);
+			for (int i=0; *(n_char+i) != '\0'; i++)
+			{
+				n = n*10 + (*(n_char+i) - 48);
+			}
+			unsigned int count = 0;
+			for (int i=0;i<n;i++)
+			{
+				if (count%16 == 0)
+				       printf("\n%x: 0x", (address+i));
+				else if (count%4 == 0)
+				       printf("     0x");
+				printf("%x", pmem[address+i]);
+				count++;
+			}
 		}
 	}
 	return 0;
