337953acaf611458062f9cdbe79b148603f1f644
tracer-ics2019 <tracer@njuics.org>
2019-09-18 01:03:41 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:03:41 up  1:45,  5 users,  load average: 0.30, 0.33, 0.26 6911cfe3487aa75c4dcd66f8d4c83664c383ea1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -29,7 +29,7 @@ static struct rule {
   {"/", '/'},           // divide
   {"\\*", '*'},         // multiply
   {"\\+", '+'},         // plus
-  {"-", '-'},           // minus
+  {"- +", '-'},           // minus
   {"==", TK_EQ}         // equal
 };
 
