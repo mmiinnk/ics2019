ce6d7702bb7964b7c4e4787f2c3514c1afc35d4a
tracer-ics2019 <tracer@njuics.org>
2019-09-21 23:01:22 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:01:22 up 10:16,  5 users,  load average: 0.30, 0.24, 0.26 72cd8d85cc761e4580276714d4da3ec7129f4842

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -18,7 +18,7 @@ uint32_t to_num(char *result){
 }
 
 int main(int argc, char *argv[]) {
-/*  FILE *fp = NULL;
+  FILE *fp = NULL;
   fp = fopen("/home/victor/ics2019/nemu/tools/gen-expr/input","r");
   
   char result[10];
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
 
   if (expr_right)
 	  printf("Bingo! You made it!!!\n");
-*/
+
 
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
