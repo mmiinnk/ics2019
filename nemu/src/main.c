d354a9b757064617b93af0c6f3acd492d5ea760c
tracer-ics2019 <tracer@njuics.org>
2019-09-21 21:15:50 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:15:50 up  8:30,  7 users,  load average: 0.17, 0.27, 0.33 620a80ba758ca41f9741c027b5eb505be5b5db77

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "monitor/debug/expr.c"
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
-//uint32_t to_num(char *result){
-
-//}
-
-
-
+uint32_t to_num(char *result){
+	int true_num = 0;
+	for (int i=0;result[i]!='\0';i++){
+		true_num = true_num*10 + result[i] - '0';
+	}
+	return true_num;
+}
 
 int main(int argc, char *argv[]) {
-  /*
   FILE *fp = NULL;
   fp = fopen("/home/victor/ics2019/nemu/tools/gen-expr/input","r");
   
@@ -22,6 +24,8 @@ int main(int argc, char *argv[]) {
   fseek(fp, 0L, SEEK_SET);
   long start = ftell(fp);
 
+  bool expr_right = true;
+
   while(end != start){
   	fscanf(fp, "%s", result);
   	fgets(expression, 65536, (FILE*)fp);
@@ -29,24 +33,20 @@ int main(int argc, char *argv[]) {
 	//printf("result: %s\n", result);
 	//printf("expression: %s\n", expression);
 	
-	bool* success = (bool*)malloc(1);
+	bool* success = (bool*) malloc(1);
 	uint32_t myAnswer = expr(expression, success);
 	if (success){
 		uint32_t trueAnswer = to_num(result);
+		if (trueAnswer != myAnswer)
+			expr_right = false;
 	}
-
 	start = ftell(fp);
-
   }
-  
-  //bool* success = (bool*)malloc(1);
-  //expr(expression, success);
-  //if (success){
-//
-  //}
-
   fclose(fp);
-  */
+
+  if (expr_right)
+	  printf("Bingo! You made it!!!\n");
+
 
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
