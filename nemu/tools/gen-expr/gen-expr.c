c9d68b410700b187338e7a9a453b4ed560fb7154
tracer-ics2019 <tracer@njuics.org>
2019-09-22 21:35:33 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:35:33 up 12 min,  4 users,  load average: 0.11, 0.05, 0.03 3a93a662772a0a99b5efb3591c52539fde41e60b

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -103,29 +103,6 @@ static char *code_format =
 "  return 0; "
 "}";
 
-/*
-void executeCMD(const char *cmd, char *result)   
-{
-       	char buf_ps[1024];   
-	char ps[1024]={0};   
-	FILE *ptr;
-	strcpy(ps, cmd);   
-	if((ptr=popen(ps, "r"))!=NULL){
-	        while(fgets(buf_ps, 1024, ptr)!=NULL)   
-	        {
-	          strcat(result, buf_ps);   
-	              if(strlen(result)>1024)   
-                     break;   
-              }   
-	        pclose(ptr);   
-	        ptr = NULL;   
-	    }   
-        else  
-    {
-           printf("popen %s error\n", ps);   
-        } 
-}
-*/
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
