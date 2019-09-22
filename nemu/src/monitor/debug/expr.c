20699a9873558ae1f7e9479b3f36c339db238edd
tracer-ics2019 <tracer@njuics.org>
2019-09-22 22:10:43 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:10:43 up 47 min,  5 users,  load average: 0.00, 0.00, 0.00 73088f7241383d2de7c5795fb5d891dfdb91cf64

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -238,7 +238,7 @@ uint32_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   for (int i=0;i<nr_token;i++){
-	  if (tokens[i].type == '-' && (i == 0 || tokens[i-1].str[0] == '\0'))
+	  if (tokens[i].type == '-' && (i == 0 || tokens[i-1].type == '+' || tokens[i-1].type == '-' || tokens[i-1].type == '*' || tokens[i-1].type == '/'))
 		  tokens[i].type = NEGATIVE;
   }
 
