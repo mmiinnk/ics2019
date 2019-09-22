4bf1f75e4757cce8793ef4aaeeb12fba07f10f93
tracer-ics2019 <tracer@njuics.org>
2019-09-22 22:15:08 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:15:08 up 52 min,  5 users,  load average: 0.01, 0.01, 0.00 87ac8d64844fb9fb8e1755db393de1c632f2c69

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -62,6 +62,7 @@ int main(int argc, char *argv[]) {
 		uint32_t trueAnswer = to_num(result);
 		if (trueAnswer != myAnswer)
 			expr_right = false;
+		printf("%d\n",myAnswer);
 	}
 	start = ftell(fp);
   }
