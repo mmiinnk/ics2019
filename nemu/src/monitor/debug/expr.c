b899d57c40d4c0c450f6573f2a89d5c1a82f10e1
tracer-ics2019 <tracer@njuics.org>
2019-09-23 12:54:03 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  12:54:03 up 26 min,  4 users,  load average: 0.05, 0.05, 0.05 868e1a5082f8cefc6150f2a4994c1cd020ed26f2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -230,8 +230,8 @@ uint32_t eval(int p, int q){
 					  printf("p = %d\n",p);
 					  printf("q = %d\n",q);
 					  printf("op = %d\n",op);
-					  printf("tokens[p] = %s\n",tokens[p].str);
-					  printf("tokens[q] = %s\n",tokens[q].str);
+					  printf("p.type = %u; p.str = %s\n",tokens[p].type,tokens[p].str);
+					  printf("q.type = %u; q.str = %s\n",tokens[q].type,tokens[q].str);
 					  //*success = false;
 					  assert(0);
 				  } return val1 / val2;
