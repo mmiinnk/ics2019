9e8ccc685918142331d4f974046dd6d5db156001
tracer-ics2019 <tracer@njuics.org>
2019-10-14 22:02:42 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:02:42 up 8 min,  1 user,  load average: 0.08, 0.09, 0.07 2a432837e85ca61e12afb7468aefc631e96b7459

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -15,9 +15,10 @@ make_EHelper(and) {
 
 make_EHelper(xor) {
   rtl_xor(&id_dest->val, &id_dest->val, &id_src->val);
-  if (id_dest->type == OP_TYPE_REG){
-    cpu.gpr[id_dest->reg]._32 = id_dest->val;
-  }
+  operand_write(id_dest, &id_dest->val);
+  //if (id_dest->type == OP_TYPE_REG){
+  //  cpu.gpr[id_dest->reg]._32 = id_dest->val;
+  //}
 
   print_asm_template2(xor);
 }
