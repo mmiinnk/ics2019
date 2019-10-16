8f2b2430e95658cf4eca458004fb2eaeb0fcb9c9
tracer-ics2019 <tracer@njuics.org>
2019-10-17 00:30:11 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:30:11 up  6:15, 12 users,  load average: 0.10, 0.05, 0.01 7bc101cd65dd3faadb5e91175c815dfde05144b1

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -2,30 +2,38 @@
 #include "cc.h"
 
 make_EHelper(test) {
-  TODO();
+  rtl_and(&s0, &id_dest->val, &id_src->val);
+  s1 = 0;
+  rtl_set_CF(&s1);
+  rtl_set_OF(&s1);
+  rtl_update_ZFSF(&s0, decinfo.width);
 
   print_asm_template2(test);
 }
 
 make_EHelper(and) {
   rtl_and(&s0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &s0);
   s1 = 0;
   rtl_set_CF(&s1);
   rtl_set_OF(&s1);
+  rtl_update_ZFSF(&s0, decinfo.width);
+
+  operand_write(id_dest, &s0);
 
   print_asm_template2(and);
 }
 
 make_EHelper(xor) {
   rtl_xor(&s0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &s0);
   //if (id_dest->type == OP_TYPE_REG){
   //  cpu.gpr[id_dest->reg]._32 = id_dest->val;
   //}
   s1 = 0;
   rtl_set_CF(&s1);
   rtl_set_OF(&s1);
+  rtl_update_ZFSF(&s0, decinfo.width);
+
+  operand_write(id_dest, &s0);
 
   print_asm_template2(xor);
 }
