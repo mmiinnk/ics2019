98097e4988364c0cc0d7d693d627fc58462384d8
tracer-ics2019 <tracer@njuics.org>
2019-10-18 00:38:05 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:38:05 up  6:29, 11 users,  load average: 0.00, 0.02, 0.00 d8d71fcc56a85f2893137eaa4ea4e58dbf7e20e0

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -31,7 +31,7 @@ make_group(gp2,
 
 /* 0xf6, 0xf7 */
 make_group(gp3,
-    EX(test), EMPTY, EX(not), EMPTY,
+    IDEX(I, test), EMPTY, EX(not), EMPTY,
     EMPTY, EMPTY, EMPTY, EX(idiv))
 
 /* 0xfe */
@@ -113,7 +113,7 @@ static OpcodeEntry opcode_table [512] = {
   /* 0xe8 */	IDEX(J, call), EMPTY, EMPTY, IDEXW(J, jmp, 1),
   /* 0xec */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf0 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xf4 */	EMPTY, EMPTY, IDEXW(I2E, gp3, 1), IDEX(E, gp3),
+  /* 0xf4 */	EMPTY, EMPTY, IDEXW(E, gp3, 1), IDEX(E, gp3),
   /* 0xf8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xfc */	EMPTY, EMPTY, IDEXW(E, gp4, 1), IDEX(E, gp5),
 
