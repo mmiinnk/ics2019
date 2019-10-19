e2c8b9adacd2120b80f218042d797e948ea7ecdf
tracer-ics2019 <tracer@njuics.org>
2019-10-19 11:08:31 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  11:08:31 up 44 min, 11 users,  load average: 0.00, 0.02, 0.05 a385801aa54fd437cc2164b63d932b7d3d22c83f

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -10,7 +10,7 @@ void set_prepos_zero(rtlreg_t* dest, rtlreg_t* src1, rtlreg_t* src2){
 make_EHelper(add) {
   rtl_add(&s1, &id_dest->val, &id_src->val);
 
-  //operand_write(id_dest, &s1);
+  operand_write(id_dest, &s1);
 
   if (decinfo.width < 4){
     set_prepos_zero(&s1, &id_dest->val, &id_src->val);
@@ -39,7 +39,7 @@ make_EHelper(sub) {
   rtl_sub(&s1, &id_dest->val, &id_src->val);
 
   //write dest = s1
-  operand_write(id_dest, &s1);
+  //operand_write(id_dest, &s1);
   
   //将前面的位置为0
   if (decinfo.width < 4){
