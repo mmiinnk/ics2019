e3629b0e3176dd7f7548abf0b6792259e34ec8eb
tracer-ics2019 <tracer@njuics.org>
2019-10-21 00:58:51 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:58:51 up  9:05,  1 user,  load average: 0.58, 0.57, 0.43 9e2b790978ee22947ad748c346a62d94a350a427

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -6,24 +6,51 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   union isa_gdb_regs r;
   ref_difftest_getregs(&r);
 
-  if (r.eax != ref_r->eax)
+  if (r.eax != ref_r->eax){
+    printf("Eax was Wrong!\n");
     return false;
-  if (r.ecx != ref_r->ecx)
+  }
+    
+  if (r.ecx != ref_r->ecx){
+    printf("Ecx was Wrong!\n");
     return false;
-  if (r.edx != ref_r->edx)
-    return false; 
-  if (r.ebx != ref_r->ebx)
+  }
+    
+  if (r.edx != ref_r->edx){
+    printf("Edx was Wrong!\n");
     return false;
-  if (r.esp != ref_r->esp)
+  }
+
+  if (r.ebx != ref_r->ebx){
+    printf("Ebx was Wrong!\n");
     return false;
-  if (r.ebp != ref_r->ebp)
+  }
+    
+  if (r.esp != ref_r->esp){
+    printf("Esp was Wrong!\n");
     return false;
-  if (r.esi != ref_r->esi)
+  }
+    
+  if (r.ebp != ref_r->ebp){
+    printf("Ebp was Wrong!\n");
     return false;
-  if (r.edi != ref_r->edi)
+  }
+    
+  if (r.esi != ref_r->esi){
+    printf("Esi was Wrong!\n");
     return false;
-  if (r.eip != ref_r->pc)
+  }
+    
+  if (r.edi != ref_r->edi){
+    printf("Edi was Wrong!\n");
     return false;
+  }
+    
+  if (r.eip != ref_r->pc){
+    printf("PC was Wrong!\n");
+    return false;
+  }
+    
   return true;
 }
 
