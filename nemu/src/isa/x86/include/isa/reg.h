0b6529038fb60a005ee80cd8619e4931fa81221e
tracer-ics2019 <tracer@njuics.org>
2019-10-27 15:02:04 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:02:04 up  4:55,  1 user,  load average: 0.33, 0.24, 0.19 f9b1f8d8ee812b52836e789f998d00b298fbd6ec

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -41,18 +41,15 @@ typedef struct {
 
   vaddr_t pc;
 
-  union{
-      struct bs{
-      unsigned CF:1;
-      unsigned vacancy1:5;
-      unsigned ZF:1;
-      unsigned SF:1;
-      unsigned vacancy2:3;
-      unsigned OF:1;
-      unsigned vacancy3:20;
-    } eflags;
-    rtlreg_t eflag;
-  };
+  struct bs{
+    unsigned CF:1;
+    unsigned vacancy1:5;
+    unsigned ZF:1;
+    unsigned SF:1;
+    unsigned vacancy2:3;
+    unsigned OF:1;
+    unsigned vacancy3:20;
+  } eflags;
 
 } CPU_state;
 
