b0f79bf26d94cd69fdf71f4291a5dd10fe53ba16
tracer-ics2019 <tracer@njuics.org>
2019-10-31 09:41:31 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:41:31 up 16:24,  6 users,  load average: 0.10, 0.14, 0.25 17989b0f2fc6b6f170c896997842242abde80c12

--- nexus-am/tests/cputest/tests/hello-str.c
+++ nexus-am/tests/cputest/tests/hello-str.c
@@ -5,9 +5,11 @@ char buf[128];
 int main() {
 	sprintf(buf, "%s", "Hello world!\n");
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
+	printf("Hello World Finished!\n");
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
+	printf("d + d = d Finished\n");
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
 	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
