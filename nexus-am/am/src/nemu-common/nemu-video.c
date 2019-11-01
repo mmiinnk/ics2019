04542133e0a560c608f02a9217142814615de412
181240068-谢云涌 <181240068@smail.nju.edu.cn>
2019-11-01 13:07:11 +0800

    implement _DEVREG_VIDEO_INFO and _DEVREG_VIDEO_FBCTL

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -6,8 +6,9 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      int width_height = inl(SCREEN_ADDR);
+      info->width = width_height & 0xffff;
+      info->height = width_height >> 16;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -18,6 +19,15 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+      int width = screen_width();
+      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+      uint32_t *start = fb + ctl->y*width + ctl->x;
+      for (int y = 0; y < ctl->h; y++){
+        for (int x = 0; x < ctl->w; x++){
+          start[y*width + x] = ctl->pixels[y * ctl->w + x];
+        }
+      }
 
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
@@ -29,9 +39,9 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_vga_init() {
-  //int i;
-  //int size = screen_width() * screen_height();
-  //uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  //for (i = 0; i < size; i ++) fb[i] = i;
-  //draw_sync();
+  int i;
+  int size = screen_width() * screen_height();
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < size; i ++) fb[i] = i;
+  draw_sync();
 }
