ff26e73a584d332256e51739e81ce8888d1faf49
tracer-ics2019 <tracer@njuics.org>
2019-11-08 00:40:09 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:40:09 up  2:54,  1 user,  load average: 0.13, 0.27, 0.26 730a59bcc17f4492790ba5b94e57eadde7a8d09a

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -20,7 +20,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   switch (subcode & 0xe) {
     case CC_O:  *dest = (OF == 1) ? 1 : 0; break;
     //case CC_NO: *dest = (OF == 0) ? 1 : 0; break;
-    case CC_B:  *dest = (CF == 1 && ZF == 0) ? 1 : 0; break;
+    case CC_B:  *dest = (CF == 1) ? 1 : 0; break;
     //case CC_NB: *dest = (CF == 0) ? 1 : 0; break;
     case CC_E:  *dest = (ZF == 1) ? 1 : 0; break;
 	  //case CC_NE: *dest = (ZF == 0) ? 1 : 0; break;
@@ -30,7 +30,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 	  //case CC_NS: *dest = (SF == 0) ? 1 : 0; break;
 	//case CC_P:  *dest = (PF == 1) ? 1 : 0; break;
 	//case CC_NP: *dest = (PF == 0) ? 1 : 0; break;
-    case CC_L:  *dest = (SF != OF && ZF == 0) ? 1 : 0; break;
+    case CC_L:  *dest = (SF != OF) ? 1 : 0; break;
 	  //case CC_NL: *dest = (SF == OF) ? 1 : 0; break;
     case CC_LE: *dest = (SF != OF || ZF == 1) ? 1 : 0; break;
     //case CC_NLE:*dest = (ZF == 0 && SF == OF) ? 1 : 0; break;
