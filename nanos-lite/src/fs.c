2495533d8785368644481b14185e97c2f398e97a
tracer-ics2019 <tracer@njuics.org>
2019-11-23 01:47:54 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:47:54 up  3:52,  2 users,  load average: 0.13, 0.32, 0.29 4c5280f6f8eb71bf1a472f9db97c421e6a53c984

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -63,10 +63,13 @@ ssize_t fs_read(int fd, void *buf, size_t len){
   Finfo *File = &file_table[fd];
   
   if ((File->open_offset + len) > File->size){
-    printf("File size = %d\n", File->size);
-    printf("File->open_offset = %d, len = %d\n", File->open_offset, len);
-    printf("The open_offset will be over the Bound of File!\n");
-    return -1;
+    //printf("File size = %d\n", File->size);
+    //printf("File->open_offset = %d, len = %d\n", File->open_offset, len);
+    //printf("The open_offset will be over the Bound of File!\n");
+    for (int i = 0; i < (File->size - File->open_offset); ++i){
+      File->open_offset += ramdisk_read(buf, File->disk_offset + File->open_offset, 1);
+    }
+    return File->size - File->open_offset;
   }
   File->open_offset += ramdisk_read(buf, File->disk_offset + File->open_offset, len);
   return len;
