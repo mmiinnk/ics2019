e54632111303c0b31e82156b900f436e6c35e4bf
tracer-ics2019 <tracer@njuics.org>
2019-11-24 01:11:38 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:11:38 up 1 day,  3:16,  1 user,  load average: 0.08, 0.15, 0.15 f18bea86eb24988498149c12633f1f9a4318a266

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -13,8 +13,8 @@ _Context* __am_irq_handle(_Context *c) {
   _Context *next = c;
   if (user_handler) {
     _Event ev = {0};
-    //int x = c->irq;
-    //printf("%d\n", x);
+    int x = c->irq;
+    printf("c->irq = %d\n", x);
     switch (c->irq) {
       case 0x81: ev.event = _EVENT_YIELD; break;
       case 0x80: ev.event = _EVENT_SYSCALL; break;
