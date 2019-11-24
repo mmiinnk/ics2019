0075d5d784bf663ca315fcc09530878a33034264
tracer-ics2019 <tracer@njuics.org>
2019-11-24 17:59:36 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:59:36 up  7:02,  1 user,  load average: 0.58, 0.40, 0.27 1a197ca63871f410b9a692d34d5e2239ff57c5e1

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -50,9 +50,11 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   Log("Reach fb_write");
   offset /= 4;
-  int x = offset % screen_width();
-  int y = offset / screen_width();
-  draw_rect((uint32_t *)buf, x, y, len/4, 1);
+  for (int i = 0; i < len/4; i++){
+    int x = (offset + i) % screen_width();
+    int y = (offset + i) / screen_width();
+    draw_rect((uint32_t *)buf + i, x, y, 1, 1);
+  }
   return len;
 }
 
