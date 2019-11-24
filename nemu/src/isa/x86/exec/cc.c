857bf411121f0d8ce1aafcd746608b44f101dd5f
tracer-ics2019 <tracer@njuics.org>
2019-11-24 17:47:25 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:47:25 up  6:50,  1 user,  load average: 0.15, 0.22, 0.18 5b48c3bdda216abd73ed0dc518919ced36c2f628

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -30,7 +30,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 	  //case CC_NS: *dest = (SF == 0) ? 1 : 0; break;
 	//case CC_P:  *dest = (PF == 1) ? 1 : 0; break;
 	//case CC_NP: *dest = (PF == 0) ? 1 : 0; break;
-    case CC_L:  *dest = (SF != OF) ? 1 : 0; break;
+    case CC_L:  *dest = (SF != OF && ZF == 0) ? 1 : 0; break;
 	  //case CC_NL: *dest = (SF == OF) ? 1 : 0; break;
     case CC_LE: *dest = (SF != OF || ZF == 1) ? 1 : 0; break;
     //case CC_NLE:*dest = (ZF == 0 && SF == OF) ? 1 : 0; break;
