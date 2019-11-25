e026776f63dc54c9a929c5a5fbffe44d65f1586f
tracer-ics2019 <tracer@njuics.org>
2019-11-25 23:30:22 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:30:22 up 1 min,  1 user,  load average: 0.89, 0.31, 0.11 d41983e69564fb35ed6863c8f00f4419b2b8a0d6

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -55,10 +55,11 @@ int fs_open(const char *pathname, int flags, int mode){
   //printf("%s\n", pathname);
   //printf("NR_FILES = %d\n", NR_FILES);
   for (int i = 0; i < NR_FILES; i++){
-    printf("i = %d\n", i);
-    if (i == 44){
+    //printf("i = %d\n", i);
+    /*if (i == 44){
       printf("%s\n", file_table[i].name);
     }
+    */
     //printf("%s\n", file_table[i].name);
     if (strcmp(file_table[i].name, pathname) == 0){
       file_table[i].open_offset = 0;
