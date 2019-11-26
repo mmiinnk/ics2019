68cd2fbf4d24f5c7bbf8635499dd697ead183806
tracer-ics2019 <tracer@njuics.org>
2019-11-26 18:30:18 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:30:18 up 19:01,  1 user,  load average: 0.45, 0.35, 0.30 37eb6c4cf405d9ba3da14368aab1854061991a40

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -46,9 +46,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       for (int j = 0; j < (p->p_memsz - p->p_filesz); ++j){
         buf[p->p_filesz + j] = 0;
       }
-      printf("p_memsz = 0x%x\n", p->p_memsz);
+      //printf("p_memsz = 0x%x\n", p->p_memsz);
       printf("p->vaddr = 0x%x\n", p->p_vaddr);
-      printf("size of buf = 0x%x\n", sizeof(buf));
+      printf("size of buf = 0x%x\n\n", sizeof(buf));
       memcpy((void *)p->p_vaddr, buf, p->p_memsz);
     }
   }
