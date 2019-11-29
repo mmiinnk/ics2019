d419a73df40aa2144640ceecf3276b7e1d3d0384
tracer-ics2019 <tracer@njuics.org>
2019-11-29 21:00:06 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:00:06 up 2 days,  4:23,  1 user,  load average: 0.17, 0.36, 0.49 29186efa5bce1fcc29bb76a7dcf3f3a9992bd2

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -14,30 +14,29 @@ make_EHelper(jcc) {
   rtl_setcc(&s0, cc);
   rtl_li(&s1, 0);
   rtl_jrelop(RELOP_NE, &s0, &s1, decinfo.jmp_pc);
-
+  
   print_asm("j%s %x", get_cc_name(cc), decinfo.jmp_pc);
 }
 
 make_EHelper(jmp_rm) {
-  decinfo.jmp_pc = id_dest->val;
-  rtl_j(decinfo.jmp_pc);
-  //rtl_j(id_dest->val);
+  rtl_jr(&id_dest->val);
 
   print_asm("jmp *%s", id_dest->str);
 }
 
 make_EHelper(call) {
-  rtl_push(&decinfo.seq_pc);
   // the target address is calculated at the decode stage
+  s0 = decinfo.seq_pc;
+  rtl_push(&s0);
   rtl_j(decinfo.jmp_pc);
 
   print_asm("call %x", decinfo.jmp_pc);
 }
 
 make_EHelper(ret) {
-  rtl_pop(&decinfo.jmp_pc);
-  rtl_j(decinfo.jmp_pc);
-
+  rtl_pop(&s0);
+  rtl_j(s0);
+  
   print_asm("ret");
 }
 
@@ -48,15 +47,9 @@ make_EHelper(ret_imm) {
 }
 
 make_EHelper(call_rm) {
-  /*
-  if (decinfo.isa.is_operand_size_16){
-    decinfo.jmp_pc = (id_dest->val)&(0xffff);
-  }else{
-    decinfo.jmp_pc = id_dest->val;
-  }
-  */
-  rtl_push(&decinfo.seq_pc);
-  rtl_jr(&id_dest->val);
+  s0 = decinfo.seq_pc;
+  rtl_push(&s0);
+  rtl_j(id_dest->val);
 
   print_asm("call *%s", id_dest->str);
 }
