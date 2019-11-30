5934f81fbc7324bf3901f75f0f0233c79812ba6f
tracer-ics2019 <tracer@njuics.org>
2019-12-01 00:05:35 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:05:35 up 45 min,  1 user,  load average: 0.22, 0.35, 0.41 d49455ee122600d922333a6ab0b845b13d08171

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -1,5 +1,4 @@
 #include "rtl/rtl.h"
-//#include "../../../../nexus-am/am/include/x86.h"
 
 void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
@@ -12,10 +11,10 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
     assert(0);
   }
   uint32_t p = cpu.idtr.baseAddress + NO*8;
-  //uint32_t gatedesc_lo = vaddr_read(p, 4);
-  //uint32_t gatedesc_hi = vaddr_read(p + 4, 4);
-  //rtl_j((gatedesc_lo & 0xffff) + (gatedesc_hi & 0xffff0000));
-  rtl_j(vaddr_read(p, 2) + (vaddr_read(p + 6, 2) << 16));
+  uint32_t gatedesc_lo = vaddr_read(p, 4);
+  uint32_t gatedesc_hi = vaddr_read(p + 4, 4);
+  rtl_j((gatedesc_lo & 0xffff) + (gatedesc_hi & 0xffff0000));
+  //rtl_j(vaddr_read(p, 2) + (vaddr_read(p + 6, 2) << 16));
 }
 
 bool isa_query_intr(void) {
