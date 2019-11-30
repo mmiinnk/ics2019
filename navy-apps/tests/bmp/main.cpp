edc5898a6c9f9c31cefd0f0971f11032d60eac37
tracer-ics2019 <tracer@njuics.org>
2019-12-01 01:36:53 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:36:53 up  2:16,  1 user,  load average: 2.00, 1.46, 0.72 8254db9963047029345d1b56f8deb1d9519a6d73

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -16,7 +16,9 @@ int main() {
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
   printf("NDL_DramRect successfully!\n");
   NDL_Render();
+  printf("Render successfully!\n");
   NDL_CloseDisplay();
+  printf("ALL Succeeded!\n");
   while (1);
   return 0;
 }
