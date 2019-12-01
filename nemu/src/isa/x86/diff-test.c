d193732906786ca64d134e6a04199daa235fdc83
tracer-ics2019 <tracer@njuics.org>
2019-12-01 23:29:52 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:29:52 up  9:51,  1 user,  load average: 0.16, 0.19, 0.11 b2715ab4f826195971d2a8fc9506e1f244c2daf

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,6 +2,17 @@
 #include "monitor/diff-test.h"
 //#include "../../../tools/qemu-diff/src/isa/x86/include/isa.h"
 
+// 从DUT host memory的`src`处拷贝`n`字节到REF guest memory的`dest`处
+void difftest_memcpy_from_dut(paddr_t dest, void *src, size_t n);
+// 获取REF的寄存器状态到`r`
+void difftest_getregs(void *r);
+// 设置REF的寄存器状态为`r`
+void difftest_setregs(const void *r);
+// 让REF执行`n`条指令
+void difftest_exec(uint64_t n);
+// 初始化REF的DiffTest功能
+void difftest_init();
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   //union isa_gdb_regs r;
   //ref_difftest_getregs(&r);
@@ -82,4 +93,14 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 }
 
 void isa_difftest_attach(void) {
+  difftest_memcpy_from_dut(0, (void *)0, 0x7c00);
+  difftest_memcpy_from_dut(0x100000, (void *)0x100000, PMEM_SIZE - 0x100000);
+  difftest_memcpy_from_dut(0x7e00, &cpu.idtr, 6);
+  uint8_t instruction[] = {0x0f, 0x01, 0x1D, 0x00, 0x7e, 0x00, 0x00};
+  difftest_memcpy_from_dut(0x7e40, instruction, sizeof(instruction));
+  CPU_state temp = cpu;
+  temp.pc = 0x7e40;
+  difftest_setregs(&temp);
+  difftest_exec(1);
+  difftest_setregs(&cpu);
 }
