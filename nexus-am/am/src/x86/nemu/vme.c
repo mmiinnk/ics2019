a4e482c39de66516d123ffa69bd7ec05de135c5e
tracer-ics2019 <tracer@njuics.org>
2019-12-15 19:02:05 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:02:05 up 1 day, 20:50,  1 user,  load average: 1.38, 1.63, 1.64 688d6acbb2dad72778be58b04dc336ef8ffc297a

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -84,10 +84,7 @@ int _map(_AddressSpace *as, void *va, void *pa, int prot) {
 }
 
 _Context *_ucontext(_AddressSpace *as, _Area ustack, _Area kstack, void *entry, void *args) {
-  ustack.end -= 32 * sizeof(uintptr_t);
-  for (int i=0;i<32;++i){
-      *((uintptr_t *)ustack.end+i) = 0;
-  }
+  ustack.end -= 12;
 
   _Context* c= (_Context*)(ustack.end) - 1;
   c->eip = (uintptr_t)entry;
