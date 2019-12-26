da0bf4c50c64f492309e638c082e1d876f1408e4
tracer-ics2019 <tracer@njuics.org>
2019-12-27 07:53:34 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  07:53:34 up 21:57,  3 users,  load average: 0.33, 0.34, 0.24 eda90ec6e0a2ec2135c4e7d7d97e058340eb7114

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,25 +1,34 @@
 #include "nemu.h"
 
+#define PG (cpu.CR0&0x1)
+
 paddr_t page_translate(vaddr_t vaddr){
-  TODO();
-  return 0;
+  uint32_t dir = (vaddr>>22)&0x3ff;
+  uint32_t page = (vaddr>>12)&0x3fff;
+  uint32_t offset = vaddr&0xffff;
+  paddr_t dir_entry = paddr_read(cpu.CR3 + dir, 4);
+  paddr_t pa_base = paddr_read(dir_entry + page, 4);
+  return pa_base + offset;
 }
 
 
 
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-  if ((addr&0xfff) + len > 0xfff + 1){
-    printf("Data cross the page boundary\n");
-    /*special case, handle later*/
-    assert(0);
+  if (PG == 1){
+    if ((addr&0xfff) + len > 0xfff + 1){
+      printf("Data cross the page boundary\n");
+      /*special case, handle later*/
+      assert(0);
+    }
+    else
+    {
+      paddr_t paddr = page_translate(addr);
+      return paddr_read(paddr, len);
+    }
   }
-  else
-  {
-    paddr_t paddr = page_translate(addr);
-    return paddr_read(paddr, len);
+  else{
+    return paddr_read(addr, len);
   }
-  
-  return paddr_read(addr, len);
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
