baa27843083b9f9a49e4475ca8b47b5e1ba5c71f
tracer-ics2019 <tracer@njuics.org>
2019-12-27 10:29:42 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  10:29:42 up  2:21,  1 user,  load average: 0.25, 0.22, 0.17 2cb112ef58af267ace46d3c6d092747c24553f7

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,13 +1,51 @@
 #include "nemu.h"
 
 #define PG (cpu.CR0&0x1)
+#define P(_) (_&0x1)
+#define Base_Address(_) ((_>>12)&0xfffff)
 
-paddr_t page_translate(vaddr_t vaddr){
+paddr_t page_translate(vaddr_t vaddr, bool is_write){
+  /* Get dir, page and pffset form vaddr */
   uint32_t dir = (vaddr>>22)&0x3ff;
   uint32_t page = (vaddr>>12)&0x3fff;
   uint32_t offset = vaddr&0xffff;
-  paddr_t dir_entry = paddr_read(cpu.CR3 + dir, 4);
-  paddr_t pa_base = paddr_read(dir_entry + page, 4);
+
+/*-----------------Get Dir Entry from Page Directory-----------------*/
+  paddr_t dir_context = paddr_read(cpu.CR3 + dir, 4);
+  
+  /*Check the Present Label 
+    Only if the Present Label is 1, we do the following actions.*/
+  if (P(dir_context) != 1){
+    printf("The Dir Page Miss!\n");
+    assert(P(dir_context) == 1);
+  }
+
+  // Revise the Accessed Label
+  paddr_write(cpu.CR3 + dir, dir_context | 0x20, 4);
+
+  // Get Dir Entry
+  paddr_t dir_entry = Base_Address(dir_context);
+
+/*-----------------Get PG TBL ENTRY from Page Table-----------------*/
+  paddr_t pageTable_context = paddr_read(dir_entry + page, 4);
+  /*Check the Present Label 
+    Only if the Present Label is 1, we do the following actions.*/
+  if (P(pageTable_context) != 1){
+    printf("The Dir Page Miss!\n");
+    assert(P(pageTable_context) == 1);
+  }
+
+  // Revise the Accessed Label
+  paddr_write(dir_entry + page, pageTable_context | 0x20, 4);
+
+  // Revise the Dirty Bit if Write
+  if (is_write){
+    paddr_write(dir_entry + page, pageTable_context | 0x40, 4);
+  }
+
+  // Get Physical Address Base
+  paddr_t pa_base = Base_Address(pageTable_context);
+
   return pa_base + offset;
 }
 
@@ -22,7 +60,7 @@ uint32_t isa_vaddr_read(vaddr_t addr, int len) {
     }
     else
     {
-      paddr_t paddr = page_translate(addr);
+      paddr_t paddr = page_translate(addr, false);
       return paddr_read(paddr, len);
     }
   }
@@ -32,5 +70,19 @@ uint32_t isa_vaddr_read(vaddr_t addr, int len) {
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+  if (PG == 1){
+    if ((addr&0xfff) + len > 0xfff + 1){
+      printf("Data cross the page boundary\n");
+      /*special case, handle later*/
+      assert(0);
+    }
+    else
+    {
+      paddr_t paddr = page_translate(addr, true);
+      paddr_write(paddr, data, len);
+    }
+  }
+  else{
+    paddr_write(addr, data, len);
+  }  
 }
