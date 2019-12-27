2c2bf757ad21ccbc68c303abe9eda353929b66a4
tracer-ics2019 <tracer@njuics.org>
2019-12-27 13:25:50 +0800

    >  compile 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  13:25:50 up  5:17,  1 user,  load average: 0.11, 0.17, 0.13 1ec6e95b6fb741f056ee0b1cf233f81b493077c2

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -17,7 +17,7 @@ paddr_t page_translate(vaddr_t vaddr, bool is_write){
   //printf("CR3 = 0x%x\n", CR3.val);
   //printf("page_directory_base = 0x%x\n", CR3.page_directory_base);
 
-/*-----------------Get Dir Entry from Page Directory-----------------*/
+/*--------------------------------------Get Dir Entry from Page Directory--------------------------------------*/
   paddr_t dir_context = paddr_read((cpu.CR3 & ~0xfff) + dir*4, 4);
   printf("dir_context = 0x%x\n", dir_context);
   
@@ -29,12 +29,13 @@ paddr_t page_translate(vaddr_t vaddr, bool is_write){
   }
 
   // Revise the Accessed Label
-  paddr_write(cpu.CR3, dir_context | 0x20, 4);
+  paddr_write((cpu.CR3 & ~0xfff) + dir*4, dir_context | 0x20, 4);
 
   // Get Dir Entry
   paddr_t dir_entry = Base_Address(dir_context);
 
-/*-----------------Get PG TBL ENTRY from Page Table-----------------*/
+
+/*--------------------------------------Get PG TBL ENTRY from Page Table--------------------------------------*/
   paddr_t pageTable_context = paddr_read(dir_entry + page*4, 4);
   printf("pageTable_context = 0x%x\n", pageTable_context);
   /*Check the Present Label 
@@ -45,11 +46,11 @@ paddr_t page_translate(vaddr_t vaddr, bool is_write){
   }
 
   // Revise the Accessed Label
-  paddr_write(dir_entry + page, pageTable_context | 0x20, 4);
+  paddr_write(dir_entry + page*4, pageTable_context | 0x20, 4);
 
   // Revise the Dirty Bit if Write
   if (is_write){
-    paddr_write(dir_entry + page, pageTable_context | 0x40, 4);
+    paddr_write(dir_entry + page*4, pageTable_context | 0x40, 4);
   }
 
   // Get Physical Address Base
