8a86cab040386605bdf990292c4892ef005c7e25
tracer-ics2019 <tracer@njuics.org>
2019-12-28 12:18:19 +0800

    >  run 181240068 victor Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  12:18:19 up 20:49,  1 user,  load average: 1.44, 1.34, 1.15 89bf0d3bed5efcb9e9cd07e531d6a50c58199a7

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -31,7 +31,10 @@ int mm_brk(uintptr_t brk, intptr_t increment) {
   //   current->max_brk += PGSIZE;
   // }
   // return 0;
-  printf("Precious max_brk = 0x%x\n", current->max_brk);
+  if (current->max_brk == 0){
+    current->max_brk = brk;
+  }
+  printf("Previous max_brk = 0x%x\n", current->max_brk);
   while (current->max_brk <= brk + increment){
     void *pa = new_page(1);
     _map(&current->as, (void *)current->max_brk, pa, 1);
